057c3ca673e1dafdeca680789ca45172e2dcd2ef
nanshens <619232906@qq.com>
2024-07-07 23:50:20 +0900

    init

--- web/service.py
+++ web/service.py
@@ -4,9 +4,10 @@
 # todo  查看历史结果
 # todo: 导入文件有备份
 import os
+import uuid
 
 import pandas as pd
-
+from entity import db
 from entity.executor import Executor
 from entity.validator import Validator
 
@@ -17,7 +18,10 @@ def get_validator_info_by_id(id):
 
 def get_executor_by_id(id):
     executor = Executor.query.get(id)
-    return executor.to_dict() if executor is not None else {}
+    if executor is None: return {}
+    data = executor.to_dict()
+    data['validator'] = executor.validator.to_dict(True)
+    return data
 
 def copy_validator_service(request):
     #  get by id, copy
@@ -27,18 +31,43 @@ def generate_match_entity(id):
     # todo: 读取xlsx, 或者csv 文件 生成实体属性匹配信息
     # todo: 1 自动匹配 返回结果, 手动填写, 保存.
     executor = Executor.query.get(id)
+    validator_entities = executor.validator.entities
     path = f"./data/{id}/"
+    match_entities = []
+
+    def add_entity(match_entities, headers, filename):
+        entity = {}
+        entity['id'] = str(uuid.uuid4())
+        entity['file_name'] = filename
+        entity['entity_id'] = filename
+        entity['entity_code'] = filename
+        entity['active'] = True
+        entity['attributes'] = []
+        for header in headers:
+            attribute = {}
+            attribute['id'] = str(uuid.uuid4())
+            attribute['column_name'] = header
+            attribute['attribute_id'] = header
+            attribute['attribute_code'] = header
+            attribute['active'] = True
+            entity['attributes'].append(attribute)
+        match_entities.append(entity)
+
     for filename in os.listdir(path):
         file_path = os.path.join(path, filename)
 
         if filename.endswith('.csv'):
             df = pd.read_csv(file_path)
             headers = df.columns.tolist()
-            print(headers)
+            add_entity(match_entities, headers, filename)
 
         elif filename.endswith('.xlsx'):
-            xls = pd.ExcelFile(file_path)
-            for sheet_name in xls.sheet_names:
+            xlsx = pd.ExcelFile(file_path)
+            for sheet_name in xlsx.sheet_names:
                 df = pd.read_excel(file_path, sheet_name=sheet_name)
                 headers = df.columns.tolist()
-                print(headers)
+                add_entity(match_entities, headers, filename + "_" + sheet_name)
+
+    executor.match_entities = match_entities
+    db.session.commit()
+    return executor.to_dict()
